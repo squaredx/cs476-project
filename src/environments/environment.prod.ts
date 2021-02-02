fc225c1c72f812809ff9a7f5379b418dcc1a6790
Jason Wolfe <jason.wolfe71@gmail.com>
2021-02-01 22:33:01 -0600

    Fix automated build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDcCrYIyRLm_4P9ktXIOz-t5qTcR3krPoQ',
+    authDomain: 'cs476-project.firebaseapp.com',
+    //databaseURL: '<your-database-URL>',
+    projectId: 'cs476-project',
+    storageBucket: 'cs476-project.appspot.com',
+    messagingSenderId: '1026847715148',
+    appId: '1:1026847715148:web:029f4400322f80faec69e9',
+    //measurementId: '<your-measurement-id>'
+  }
 };
